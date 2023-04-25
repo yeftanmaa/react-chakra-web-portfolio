66d13b237852b14b5a041e9e487f37705db1b973
Nathan Johanes Yefta <102526580+yeftanmaa@users.noreply.github.com>
2023-04-25 09:08:28 +0700

    DEV-004: Continue About Page
    - Add text-selection theming;
    - Add table in about page;

--- src/components/routes/about.jsx
+++ src/components/routes/about.jsx
@@ -1,5 +1,6 @@
-import { Container, Text, Box, Image } from "@chakra-ui/react";
+import { Container, Text, Box, Image, TableContainer, Table, Tbody, Tr, Td } from "@chakra-ui/react";
 import React from "react";
+import '../styles/about.css';
 
 const About = () => {
     return (
@@ -15,10 +16,34 @@ const About = () => {
                     <Image src="/images/photo.jpg" alt='Johanes Yefta' width={"100%"} height={"100%"} />
                 </Box>
 
-                <Box maxW={700} marginTop={250}>
+                <Box maxW={700} marginTop={200} marginBottom={200}>
                     <Text fontSize={34} fontWeight={300}>
                         I enjoy finding creative ways to streamline my belongings and keep my workspace tidy and functional.
                     </Text>
+                </Box>
+
+                <Box>
+                    <TableContainer>
+                        <Table>
+                            <Tbody fontSize={24}>
+                                <Tr>
+                                   <Td className="table-data">2022-today</Td>
+                                   <Td className="table-data" fontWeight={500}>Demalia</Td>
+                                   <Td className="table-data">Web Developer</Td>
+                                </Tr>
+                                <Tr paddingTop={30}>
+                                   <Td className="table-data">2022</Td>
+                                   <Td className="table-data" fontWeight={500}>Sagara Foundation</Td>
+                                   <Td className="table-data">Front-end Specialist</Td>
+                                </Tr>
+                                <Tr>
+                                   <Td className="table-data">2021</Td>
+                                   <Td className="table-data" fontWeight={500}>Two Sisters Agency</Td>
+                                   <Td className="table-data">Graphic Designer</Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
                 </Box>
             </Container>
         </div>
