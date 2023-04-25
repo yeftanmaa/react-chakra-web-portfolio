cf34fc25b827c7f9b1fb9751f4a8d3dd88133bce
Nathan Johanes Yefta <102526580+yeftanmaa@users.noreply.github.com>
2023-04-25 21:10:57 +0700

    DEV-005: Add Client Section
    - Add clients logo;
    - Continue working on work page;
    - Separate CSS from work page;
    - Edit table inside about page;

--- src/components/routes/about.jsx
+++ src/components/routes/about.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div>
             <Container className="about-global-container" maxW={"7xl"}>
-                <Box maxW={700}>
+                <Box maxW={900}>
                     <Text className="about-title-text">
                         Currently hustling and brainstorming at a Spain-based digital marketing company.
                     </Text>
@@ -16,7 +16,7 @@ const About = () => {
                     <Image src="/images/photo.jpg" alt='Johanes Yefta' w={"100%"} h={"100%"} />
                 </Box>
 
-                <Box className="about-title-wrapper" maxW={700}>
+                <Box className="about-title-wrapper" maxW={750}>
                     <Text className="about-title-text">
                         I enjoy finding creative ways to streamline my belongings and keep my workspace tidy and functional.
                     </Text>
@@ -26,25 +26,32 @@ const About = () => {
                     <TableContainer>
                         <Table>
                             <Tbody className="table-body" fontSize={24}>
-                                <Tr>
-                                   <Td className="table-data">2022-today</Td>
-                                   <Td className="table-data" fontWeight={500}>Demalia</Td>
-                                   <Td className="table-data">Web Developer</Td>
+                                <Tr className="table-first-row">
+                                   <Td padding={"50px 0"} borderBottom={"2px solid black"}>2022-today</Td>
+                                   <Td fontWeight={500} borderBottom={"2px solid black"}>Demalia</Td>
+                                   <Td borderBottom={"2px solid black"}>Web Developer</Td>
                                 </Tr>
                                 <Tr>
-                                   <Td className="table-data">2022</Td>
-                                   <Td className="table-data" fontWeight={500}>Sagara Foundation</Td>
-                                   <Td className="table-data">Front-end Specialist</Td>
+                                   <Td padding={"50px 0"} borderBottom={"2px solid black"}>2022</Td>
+                                   <Td fontWeight={500} borderBottom={"2px solid black"}>Sagara Foundation</Td>
+                                   <Td borderBottom={"2px solid black"}>Front-end Specialist</Td>
                                 </Tr>
                                 <Tr>
-                                   <Td className="table-data">2021</Td>
-                                   <Td className="table-data" fontWeight={500}>Two Sisters Agency</Td>
-                                   <Td className="table-data">Graphic Designer</Td>
+                                   <Td padding={"50px 0"} borderBottom={"2px solid black"}>2021</Td>
+                                   <Td fontWeight={500} borderBottom={"2px solid black"}>Two Sisters Agency</Td>
+                                   <Td borderBottom={"2px solid black"}>Graphic Designer</Td>
                                 </Tr>
                             </Tbody>
                         </Table>
                     </TableContainer>
                 </Box>
+
+                <Box className="about-education-wrapper" maxW={750}>
+                    <Text className="about-title-text">
+                        Pursuing a Bachelor's degree in Computer Science, with a graduation date set for 2023.
+                    </Text>
+                </Box>
+
             </Container>
         </div>
     );
--- src/components/routes/work.jsx
+++ src/components/routes/work.jsx
@@ -1,6 +1,58 @@
+import React from "react";
+import { Container, Box, Text, HStack, Image, Card, CardBody, CardFooter, Heading, IconButton } from "@chakra-ui/react";
+import '../styles/work.css';
+import { BsArrowRightCircle } from 'react-icons/bs';
+
 const Work = () => {
     return (
-        <div>This is work page</div>
+        <div>
+            <Container className="work-global-container" maxW={"7xl"}>
+                <Box>
+                    <Text className="work-title-wrapper" maxW={820}>
+                    My experience over the past years reached from mobility of digital marketing, university web project and graphic design project.
+                    </Text>
+                </Box>
+
+                <HStack className="work-clients-wrapper">
+                    <Box className="work-clients-image">
+                        <Image src="/images/two-sisters.png" objectFit={"cover"} />
+                    </Box>
+                    <Box className="work-clients-image">
+                        <Image src="/images/khayalan-studio.png" objectFit={"cover"} />
+                    </Box>
+                    <Box width={100}>
+                        <Image src="/images/ODA.png" objectFit={"cover"} />
+                    </Box>
+                    <Box className="work-clients-image">
+                        <Image src="/images/Demalia.png" objectFit={"cover"} />
+                    </Box>
+                    <Box className="work-clients-image">
+                        <Image src="/images/Familist.png" objectFit={"cover"} />
+                    </Box>
+                </HStack>
+
+                <Card boxShadow="none">
+                    <CardBody padding={0}>
+                        <Image src="/images/portfolio_two-sisters.jpg"/>
+                    </CardBody>
+                    <CardFooter padding={"45px 0"} justifyContent="space-between">
+                        <Box>
+                            <Heading as="h1" fontWeight={300}>Business – Branding & Images</Heading>
+                        </Box>
+
+                        <Box className="work-portfolio-details-wrapper">
+                            <Box>
+                                <Text className="work-portfolio-text-details">2021 – today</Text>
+                                <Text className="work-portfolio-text-details">Instagram (Android & iOS)</Text>
+                            </Box>
+                            <Box>
+                                <IconButton icon={<BsArrowRightCircle size={45}/>} variant="outline" border={0} />
+                            </Box>
+                        </Box>
+                    </CardFooter>
+                </Card>
+            </Container>
+        </div>
     );
 }
  
