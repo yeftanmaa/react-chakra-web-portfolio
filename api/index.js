519e906467a1714ab98d0533cc66c47054675bcf
Johanes Yefta <102526580+yeftanmaa@users.noreply.github.com>
2024-12-10 01:18:43 +0700

    test

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ app.get("/api/testing", (req, res) => {
   res.send("Hello from the Node.js API!");
 });
 
+app.get("/api/testing22", (req, res) => {
+  res.send("Hello from the Node.js API 22s!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
